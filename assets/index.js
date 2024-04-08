1b907843da6ca4910dfe4ed503d8859e5c64c88c
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-04-08 20:48:01 +0100

    added json file and image.

--- assets/index.js
+++ assets/index.js
@@ -5,10 +5,26 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log("button clicked");
+  renderLead();
+  inputEl.value = ""; //clearing input field
 });
 
 // Render the leads in the unordered list to be in a listed item using ulEl.innerHTML
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderLead() {
+  let listItem = `
+            <li>
+                <a target='_blank' href='${inputEl.value}'>
+                    ${inputEl.value}
+                </a>
+            </li>
+        `;
+  ulEl.innerHTML += listItem;
 }
+
+// function renderLeads() {
+//     let listItems = ""
+//     for (let i = 0; i < myLeads.length; i++) {
+//         listItems += "<li>" + myLeads[i] + "</li>"
+//     }
+//     ulEl.innerHTML = listItems
+// }
